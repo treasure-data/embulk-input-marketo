9338db1f06e98d0a5e02b43e782d529ded70b420
yoshihara <h.yoshihara@everyleaf.com>
2015-07-09 15:41:50 +0900

    Add skeleton marketo activity log plugin
    To check if lead plugin can execute wihout error.

---
+++ test/embulk/input/marketo/test_activity_log.rb
@@ -0,0 +1,14 @@
+require "prepare_embulk"
+require "embulk/input/marketo/activity_log"
+
+module Embulk
+  module Input
+    module Marketo
+      class ActivityLogTest < Test::Unit::TestCase
+        def test_guess
+          pend "TODO"
+        end
+      end
+    end
+  end
+end
