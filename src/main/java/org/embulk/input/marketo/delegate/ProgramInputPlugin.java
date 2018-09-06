5531ddbda87d503882b73e1f254085474353abff
Tuan Bui <tuan@treasure-data.com>
2018-09-06 23:01:02 +0700

    Code review update

--- src/main/java/org/embulk/input/marketo/delegate/ProgramInputPlugin.java
+++ src/main/java/org/embulk/input/marketo/delegate/ProgramInputPlugin.java
@@ -29,7 +29,7 @@ import java.util.List;
 
 public class ProgramInputPlugin extends MarketoBaseInputPluginDelegate<ProgramInputPlugin.PluginTask>
 {
-    private static final DateTimeFormatter DATE_FORMATER = DateTimeFormat.forPattern(MarketoUtils.MARKETO_DATE_SIMPLE_DATE_FORMAT);
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormat.forPattern(MarketoUtils.MARKETO_DATE_SIMPLE_DATE_FORMAT);
 
     public interface PluginTask extends MarketoBaseInputPluginDelegate.PluginTask
     {
@@ -77,62 +77,64 @@ public class ProgramInputPlugin extends MarketoBaseInputPluginDelegate<ProgramIn
     {
         super.validateInputTask(task);
         // validate if query_by is selected
-        if (task.getQueryBy().isPresent() && task.getQueryBy().get() == QueryBy.TAG_TYPE) {
-            //make sure tag type and tag value are not empty
-            if (!task.getTagType().isPresent() || !task.getTagVallue().isPresent()) {
-                throw new ConfigException("tag_type and tag_value are required when query by Tag Type");
-            }
-        }
-        else if (task.getQueryBy().isPresent() && task.getQueryBy().get() == QueryBy.DATE_RANGE) {
-            // make sure earliest_updated_at is not empty
-            if (!task.getEarliestUpdatedAt().isPresent()) {
-                throw new ConfigException("`earliest_updated_at` is required when query by Date Range");
-            }
-            DateTime earliest = new DateTime(task.getEarliestUpdatedAt().get());
+        if (task.getQueryBy().isPresent()) {
+            switch(task.getQueryBy().get()) {
+            case TAG_TYPE:
+                //make sure tag type and tag value are not empty
+                if (!task.getTagType().isPresent() || !task.getTagVallue().isPresent()) {
+                    throw new ConfigException("tag_type and tag_value are required when query by Tag Type");
+                }
+                break;
+            case DATE_RANGE:
+                // make sure earliest_updated_at is not empty
+                if (!task.getEarliestUpdatedAt().isPresent()) {
+                    throw new ConfigException("`earliest_updated_at` is required when query by Date Range");
+                }
 
-            // make sure latest_updated_at is not empty
-            if (!task.getLatestUpdatedAt().isPresent()) {
-                throw new ConfigException("`latest_updated_at` is required when query by Date Range");
-            }
+                DateTime earliest = new DateTime(task.getEarliestUpdatedAt().get());
+                // make sure latest_updated_at is not empty
+                if (!task.getLatestUpdatedAt().isPresent()) {
+                    throw new ConfigException("`latest_updated_at` is required when query by Date Range");
+                }
 
-            // If incremental report
-            if (task.getIncremental()) {
-                // The very first run
-                if (!task.getReportDuration().isPresent()) {
-                    DateTime latest = new DateTime(task.getLatestUpdatedAt().get());
-                    if (latest.isAfter(DateTime.now())) {
-                        throw new ConfigException(String.format("`latest_updated_at` (%s) cannot precede the current date (%s) when incremental import",
-                                        latest.toString(DATE_FORMATER),
-                                        (DateTime.now().toString(DATE_FORMATER))));
+                // If incremental report
+                if (task.getIncremental()) {
+                    // The very first run
+                    if (!task.getReportDuration().isPresent()) {
+                        DateTime latest = new DateTime(task.getLatestUpdatedAt().get());
+                        if (latest.isAfter(DateTime.now())) {
+                            throw new ConfigException(String.format("`latest_updated_at` (%s) cannot precede the current date (%s) when incremental import",
+                                            latest.toString(DATE_FORMATTER),
+                                            (DateTime.now().toString(DATE_FORMATTER))));
+                        }
+                    }
+                    else {
+                        // Update the latestUpdatedAt for the config
+                        DateTime latest = earliest.plus(task.getReportDuration().get());
+                        // Only import until now
+                        if (latest.isAfter(DateTime.now())) {
+                            latest = DateTime.now();
+                        }
+                        task.setLatestUpdatedAt(Optional.of(latest.toDate()));
                     }
                 }
-                else {
-                    // Update the latestUpdatedAt for the config
-                    DateTime latest = earliest.plus(task.getReportDuration().get());
-                    // Only import until now
-                    if (latest.isAfter(DateTime.now())) {
-                        latest = DateTime.now();
-                    }
-                    task.setLatestUpdatedAt(Optional.of(latest.toDate()));
+
+                DateTime latest = new DateTime(task.getLatestUpdatedAt().get());
+                if (earliest.isAfter(DateTime.now())) {
+                    throw new ConfigException(String.format("`earliest_updated_at` (%s) cannot precede the current date (%s)",
+                                    earliest.toString(DATE_FORMATTER),
+                                    (DateTime.now().toString(DATE_FORMATTER))));
                 }
-            }
 
-            DateTime latest = new DateTime(task.getLatestUpdatedAt().get());
-
-            if (earliest.isAfter(DateTime.now())) {
-                throw new ConfigException(String.format("`earliest_updated_at` (%s) cannot precede the current date (%s)",
-                                earliest.toString(DATE_FORMATER),
-                                (DateTime.now().toString(DATE_FORMATER))));
-            }
-
-            if (earliest.isAfter(latest)) {
-                throw new ConfigException(String.format("Invalid date range. `earliest_updated_at` (%s) cannot precede the `latest_updated_at` (%s).",
-                                earliest.toString(DATE_FORMATER),
-                                latest.toString(DATE_FORMATER)));
-            }
-            // if filter type is selected, filter value must be presented
-            if (task.getFilterType().isPresent() && (!task.getFilterValues().isPresent() || task.getFilterValues().get().isEmpty())) {
-                throw new ConfigException("filter_value is required when selected filter_type");
+                if (earliest.isAfter(latest)) {
+                    throw new ConfigException(String.format("Invalid date range. `earliest_updated_at` (%s) cannot precede the `latest_updated_at` (%s).",
+                                    earliest.toString(DATE_FORMATTER),
+                                    latest.toString(DATE_FORMATTER)));
+                }
+                // if filter type is selected, filter value must be presented
+                if (task.getFilterType().isPresent() && (!task.getFilterValues().isPresent() || task.getFilterValues().get().isEmpty())) {
+                    throw new ConfigException("filter_value is required when selected filter_type");
+                }
             }
         }
     }
@@ -141,19 +143,21 @@ public class ProgramInputPlugin extends MarketoBaseInputPluginDelegate<ProgramIn
     protected Iterator<ServiceRecord> getServiceRecords(MarketoService marketoService, PluginTask task)
     {
         Iterable<ObjectNode> nodes = null;
-        if (!task.getQueryBy().isPresent()) {
+        if (task.getQueryBy().isPresent()) {
+            switch (task.getQueryBy().get()) {
+            case TAG_TYPE:
+                nodes = marketoService.getProgramsByTag(task.getTagType().get(), task.getTagVallue().get());
+                break;
+            case DATE_RANGE:
+                nodes = marketoService.getProgramsByDateRange(task.getEarliestUpdatedAt().get(),
+                                task.getLatestUpdatedAt().get(),
+                                task.getFilterType().orNull(),
+                                task.getFilterValues().orNull());
+            }
+        }
+        else {
             nodes = marketoService.getPrograms();
         }
-        else if (task.getQueryBy().isPresent() && task.getQueryBy().get() == QueryBy.TAG_TYPE) {
-            nodes = marketoService.getProgramsByTag(task.getTagType().get(), task.getTagVallue().get());
-        }
-        else if (task.getQueryBy().isPresent() && task.getQueryBy().get() == QueryBy.DATE_RANGE) {
-            nodes = marketoService.getProgramsByDateRange(task.getEarliestUpdatedAt().get(),
-                            task.getLatestUpdatedAt().get(),
-                            task.getFilterType().orNull(),
-                            task.getFilterValues().orNull());
-        }
-
         return FluentIterable.from(nodes).transform(MarketoUtils.TRANSFORM_OBJECT_TO_JACKSON_SERVICE_RECORD_FUNCTION).iterator();
     }
 
@@ -170,15 +174,9 @@ public class ProgramInputPlugin extends MarketoBaseInputPluginDelegate<ProgramIn
             Duration d = new Duration(earliest, latest);
             DateTime nextEarliestUpdatedAt = latest.plusSeconds(1);
 
-            configDiff.set("earliest_updated_at", nextEarliestUpdatedAt.toString(DATE_FORMATER));
-            if (task.getReportDuration().isPresent()) {
-                configDiff.set("report_duration", task.getReportDuration());
-            }
-            else {
-                configDiff.set("report_duration", Optional.of(d.getMillis()));
-            }
+            configDiff.set("earliest_updated_at", nextEarliestUpdatedAt.toString(DATE_FORMATTER));
+            configDiff.set("report_duration", task.getReportDuration().or(d.getMillis()));
         }
-
         return configDiff;
     }
 
