98a90f12f84a82f9efe5e8e726d0c8fdb3faf212
kazuki-yane <42165581+kazuki-yane@users.noreply.github.com>
2020-04-20 13:37:00 +0900

    delete underbar if prefix is empty (#1)
    * delete underbar if prefix is empty
    
    * add test

--- src/test/java/org/embulk/input/marketo/MarketoUtilsTest.java
+++ src/test/java/org/embulk/input/marketo/MarketoUtilsTest.java
@@ -56,6 +56,8 @@ public class MarketoUtilsTest
     {
         String columnName = MarketoUtils.buildColumnName("prefix", "columnName");
         assertEquals("prefix_columnName", columnName);
+        String noPrefixColumn = MarketoUtils.buildColumnName("", "columnName");
+        assertEquals("columnName", noPrefixColumn);
     }
 
     @Test
