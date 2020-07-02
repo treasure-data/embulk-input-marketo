9c8f21d4283c73b85952f74d3ede08be9197666a
Tuan Bui <xuantuan58@gmail.com>
2020-07-02 20:33:31 +0700

    Added config key supports string filter values for Custom Object

--- src/main/java/org/embulk/input/marketo/delegate/CustomObjectInputPlugin.java
+++ src/main/java/org/embulk/input/marketo/delegate/CustomObjectInputPlugin.java
@@ -1,7 +1,9 @@
 package org.embulk.input.marketo.delegate;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Optional;
 import com.google.common.collect.FluentIterable;
+import com.google.common.collect.Iterables;
 import org.apache.commons.lang3.StringUtils;
 import org.embulk.base.restclient.ServiceResponseMapper;
 import org.embulk.base.restclient.record.ServiceRecord;
@@ -17,6 +19,9 @@ import org.embulk.spi.Exec;
 import org.slf4j.Logger;
 
 import java.util.Iterator;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CustomObjectInputPlugin extends MarketoBaseInputPluginDelegate<CustomObjectInputPlugin.PluginTask>
 {
@@ -35,6 +40,10 @@ public class CustomObjectInputPlugin extends MarketoBaseInputPluginDelegate<Cust
         @Config("custom_object_filter_to_value")
         @ConfigDefault("null")
         Optional<Integer> getToValue();
+
+        @Config("custom_object_filter_values")
+        @ConfigDefault("\"\"")
+        String getCustomObjectFilterValues();
     }
 
     public CustomObjectInputPlugin()
@@ -51,16 +60,56 @@ public class CustomObjectInputPlugin extends MarketoBaseInputPluginDelegate<Cust
         if (StringUtils.isBlank(task.getCustomObjectAPIName())) {
             throw new ConfigException("`custom_object_api_name` cannot be empty");
         }
-        if (task.getToValue().isPresent() && task.getToValue().get() < task.getFromValue()) {
-            throw new ConfigException(String.format("`to_value` (%s) cannot be less than  the `from_value` (%s)", task.getToValue().get(), task.getFromValue()));
+        if (StringUtils.isBlank(task.getCustomObjectFilterValues())) {
+            if (task.getToValue().isPresent() && !isValidFilterRange(task)) {
+                throw new ConfigException(String.format("`to_value` (%s) cannot be less than the `from_value` (%s)", task.getToValue().get(), task.getFromValue()));
+            }
+        }
+        else if (refineFilterValues(task.getCustomObjectFilterValues()).isEmpty()) {
+            throw new ConfigException("`custom_object_filter_values` cannot contain empty values only");
         }
     }
+
+    private Set<String> refineFilterValues(String filterValues)
+    {
+        return Stream.of(StringUtils.split(filterValues, ",")).filter(StringUtils::isNotBlank).collect(Collectors.toSet());
+    }
+
+    private boolean isValidFilterRange(PluginTask task)
+    {
+        return task.getToValue().get() > task.getFromValue();
+    }
+
     @Override
     protected Iterator<ServiceRecord> getServiceRecords(MarketoService marketoService, PluginTask task)
     {
-        return FluentIterable
-                .from(marketoService.getCustomObject(task.getCustomObjectAPIName(), task.getCustomObjectFilterType(), task.getCustomObjectFields().orNull(), task.getFromValue(), task.getToValue().orNull()))
-                .transform(MarketoUtils.TRANSFORM_OBJECT_TO_JACKSON_SERVICE_RECORD_FUNCTION).iterator();
+        Iterable<ObjectNode> responseObj;
+        if (StringUtils.isNotBlank(task.getCustomObjectFilterValues())) {
+            Set<String> refinedValues = refineFilterValues(task.getCustomObjectFilterValues());
+            responseObj = marketoService.getCustomObject(task.getCustomObjectAPIName(), task.getCustomObjectFilterType(), refinedValues, task.getCustomObjectFields().orNull());
+        }
+        else {
+            // When `to_value` is not set, will try to import all consecutive custom objects started from `from_value`
+            responseObj = marketoService.getCustomObject(task.getCustomObjectAPIName(), task.getCustomObjectFilterType(), task.getCustomObjectFields().orNull(), task.getFromValue(), task.getToValue().orNull());
+        }
+
+        return FluentIterable.from(filterInvalidRecords(responseObj)).transform(MarketoUtils.TRANSFORM_OBJECT_TO_JACKSON_SERVICE_RECORD_FUNCTION).iterator();
+    }
+
+    /**
+     * Marketo include error in invalid records. This method will filter those records and print it to output log
+     * @param resultIt
+     * @return
+     */
+    private Iterable<ObjectNode> filterInvalidRecords(Iterable<ObjectNode> resultIt)
+    {
+        return Iterables.filter(resultIt, (obj) -> {
+            if (obj.has("reasons")) {
+                logger.warn(obj.get("reasons").toString());
+                return false;
+            }
+            return true;
+        });
     }
 
     @Override
